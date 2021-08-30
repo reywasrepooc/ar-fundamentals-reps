4d4a16696b541f10d0cfc32a3d1149355f8120e4
Cooper Sawyer <cooper.sawyer@powerhrg.com>
2021-08-30 14:31:17 -0400

    commit tree 3f55c87756367aa24fca08c09d339fc116c8a2d8

--- app/models/donation.rb
+++ app/models/donation.rb
@@ -1,3 +1,48 @@
 class Donation < ActiveRecord::Base
+  belongs_to :nonprofit
+  belongs_to :user
 
+  def self.get_last_3
+    last(3)
+  end
+
+  def self.get_id_of_2
+    find(2)
+  end
+
+  def self.get_from_first_user
+    where(user_id: 1)
+  end
+
+  def self.get_id_of_18
+    find(18)
+  end
+
+  def self.get_from_last_nonprofit
+    where(nonprofit: Nonprofit.last)
+  end
+
+  def self.order_by_donation_amount
+    order(:amount)
+  end
+
+  def self.amount_over_350
+    where("amount > 350")
+  end
+
+  def self.count_of_donations
+    count
+  end
+
+  def self.donations_to_nonprofit_with_id_of_4
+    where(nonprofit_id: 4)
+  end
+
+  def self.total_dollars_donated
+    sum(:amount)
+  end
+
+  def self.donations_by_user_with_id_of_13
+    where(user_id: 13)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,56 @@
 class User < ActiveRecord::Base
+  has_many :donations
+  has_many :nonprofits, through: :donations
 
+  def self.get_cooper
+    find_by(first_name: "Cooper")
+  end
+
+  def self.get_first_5
+    first(5)
+  end
+
+  def self.get_nj_user
+    find_by(territory: "NJ")
+  end
+
+  def self.get_id_of_9
+    find(9)
+  end
+
+  def self.get_wenhold
+    find_by(last_name: "Wenhold")
+  end
+
+  def self.get_last
+    last
+  end
+
+  def self.get_sebastian_kopp
+    find_by(first_name: "Sebastian", last_name: "Kopp")
+  end
+
+  def self.phl_users
+    where(territory: "PHL")
+  end
+
+  def self.reverse_alphabetical_by_first_name
+    order(first_name: :desc)
+  end
+
+  def self.first_5_alphabetical_by_last_name
+    order(:last_name).first(5)
+  end
+
+  def self.amount_of_phl_users
+    where(territory: "PHL").count
+  end
+
+  def self.unique_territories
+    pluck(:territory).uniq
+  end
+
+  def self.non_phl_users
+    where("territory != 'PHL'")
+  end
 end
